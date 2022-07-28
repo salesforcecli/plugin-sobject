97ee3c1d99267b8c9baf62c90160060bb0154273
mshanemc <shane.mclaughlin@salesforce.com>
2022-07-28 14:24:17 -0500

    test: minimal nuts for commands (checking messages)

---
+++ test/nuts/tab.nut.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import * as path from 'path';
+import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
+import { env } from '@salesforce/kit';
+// import { expect } from 'chai';
+
+describe('generate tab NUTs', () => {
+  let session: TestSession;
+
+  before(async () => {
+    env.setString('TESTKIT_EXECUTABLE_PATH', path.join(process.cwd(), 'bin', 'dev'));
+    session = await TestSession.create({});
+  });
+
+  after(async () => {
+    await session?.clean();
+  });
+
+  it('help should not throw', () => {
+    const command = 'generate metadata tab --help';
+    execCmd(command, { ensureExitCode: 0, cli: 'sf' }).shellOutput.stdout;
+  });
+});
