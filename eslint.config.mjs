31d88a487366c389b6b03bcd2b5c0df7251aee92
Jamie Feingold <jfeingold@salesforce.com>
2026-08-20 14:48:06 -0500

    fix: rejiggered eslint.config.mjs (W-23928423)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,14 @@
-export { default } from 'eslint-config-salesforce-typescript';
+import tsconfigs from 'eslint-config-salesforce-typescript';
+import plugin from 'eslint-plugin-sf-plugin';
+
+const configs = [
+  ...tsconfigs,
+  ...plugin.configs.recommended,
+  {
+    rules: {
+      'sf-plugin/dash-o': 'off',
+    },
+  },
+];
+
+export default configs;
